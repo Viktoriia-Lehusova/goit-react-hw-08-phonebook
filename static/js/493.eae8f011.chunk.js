25623ff3da4af42ec9e4efe0999b65faa890134b
Viktoriia-Lehusova <109483127+Viktoriia-Lehusova@users.noreply.github.com>
2023-05-08 12:31:48 +0000

    Deploying to gh-pages from @ Viktoriia-Lehusova/goit-react-hw-08-phonebook@d99834f7319822df014889baa4afe961b05534b2 🚀

---
+++ static/js/493.eae8f011.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[493],{5493:function(e,o,t){t.r(o);var n=t(890),r=t(184);o.default=function(){return(0,r.jsx)(n.Z,{variant:"body1",sx:{p:1.5},children:"Welcome to our online phonebook!"})}}}]);
+//# sourceMappingURL=493.eae8f011.chunk.js.map
